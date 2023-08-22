72ddc6b26990511280eae535a513283fc043102a
Saadman Omar Siddique <saadman.omar@gmail.com>
2023-08-22 21:29:48 +0600

    added codes from lesson 2

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
+require('dotenv').config();
+
 const express = require('express');
+const workoutRoutes = require('./routes/workouts');
 
 // express app
 const app = express();
 
-// routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'});
+// Middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
 });
 
+// routes
+app.use('/api/workouts', workoutRoutes);
+
 // listen for reqests
-app.listen(3000, () => {
-    console.log('listening on port 3000!!');
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT);
 });
